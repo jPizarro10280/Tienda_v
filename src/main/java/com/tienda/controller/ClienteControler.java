0515c599bc69f24e1f61b46458348a7c0707e084
Jostin Pizarro <jospringa@gmail.com>
2022-11-09 19:42:23 -0600

    tienda se crea controllers, models, dao, services, domains de articulo, categoria, cliente
    se crea busqueda por apellidos

--- src/main/java/com/tienda/controller/ClienteControler.java
+++ src/main/java/com/tienda/controller/ClienteControler.java
@@ -5,6 +5,7 @@ import com.tienda.service.ClienteService;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,14 +17,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 @Slf4j
 public class ClienteControler {
-    
+
     @Autowired
     private ClienteService clienteService;
+
+
     
-        @GetMapping("/cliente/listado")
+    @GetMapping("/cliente/busqueda")
+    public String buscarApellido(Model model,@Param("apellidos")String apellidos) {
+              
+        var clientes=clienteService.buscarPorApellidos(apellidos);
+        model.addAttribute("clientes", clientes);
+        
+        return "/cliente/listado";
+    }
+    
+    @GetMapping("/cliente/buscar")
+    public String buscar(Cliente cliente){
+        return "/cliente/busqueda";
+    }
+    
+
+    @GetMapping("/cliente/listado")
     public String listado(Model model) {
-        var clientes=clienteService.getClientes();
-        model.addAttribute("clientes",clientes);
+        var clientes = clienteService.getClientes();
+        //var clientes=clienteService.buscarPorNombre("Ana");
+        model.addAttribute("clientes", clientes);
         return "/cliente/listado";
     }
 
@@ -31,29 +50,23 @@ public class ClienteControler {
     public String clienteNuevo(Cliente cliente) {
         return "/cliente/modificar";
     }
-    
+
     @PostMapping("/cliente/guardar")
     public String clienteGuardar(Cliente cliente) {
         clienteService.save(cliente);
         return "redirect:/cliente/listado";
     }
-    
-    @GetMapping("/cliente/modificar/{idCliente}")
+
+    @GetMapping("/cliente/modificar/{idCliente}")//
     public String clienteModificar(Cliente cliente, Model model) {
         cliente = clienteService.getCliente(cliente);
-        model.addAttribute("cliente",cliente);
+        model.addAttribute("cliente", cliente);
         return "/cliente/modificar";
     }
-    
-    @GetMapping("/cliente/eliminar/{idCliente}")
-    public String clienteEliminar(Cliente cliente, Model model) {
+
+    @GetMapping("/cliente/eliminar/{idCliente}")//
+    public String clienteEliminar(Cliente cliente) {
         clienteService.delete(cliente);
         return "redirect:/cliente/listado";
     }
-    
-    @GetMapping("/cliente/busqueda")
-    public String clienteBusqueda(Cliente cliente, Model model){
-        clienteService.buscarPorApellidos(apellidos);
-    }
-
 }
